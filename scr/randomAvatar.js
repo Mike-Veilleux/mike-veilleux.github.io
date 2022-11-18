7c8dfc026aaf3a3469664eaad16a504f3d91cf42
Michael Veilleux <74661812+Mike-Veilleux@users.noreply.github.com>
2022-11-18 23:05:44 +0800

    Avatar in Navbar

--- scr/randomAvatar.js
+++ scr/randomAvatar.js
@@ -1,12 +1,8 @@
 function randomAvatar() {
-
-    const cardImg = document.getElementsByClassName('card-img-top');
-
-    for (const element of cardImg) {
-        const preUrl = "https://robohash.org/";
-        const randomString = Math.random().toString(36).slice(2, 7);
-        const postUrl = "?set=set1";
-        element.src = preUrl + randomString + postUrl;
-    }
+    const img = document.getElementById('random-avatar');
+    const preUrl = "https://robohash.org/";
+    const randomString = Math.random().toString(36).slice(2, 7);
+    const postUrl = "?set=set1";
+    img.src = preUrl + randomString + postUrl;
 }
 
