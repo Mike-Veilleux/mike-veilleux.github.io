d8cc74829892d72e58fa9c67a0464780e63280e2
Michael Veilleux <74661812+Mike-Veilleux@users.noreply.github.com>
2022-11-26 23:06:20 +0800

    avatar script updated

--- scr/randomAvatar.js
+++ scr/randomAvatar.js
@@ -1,10 +1,27 @@
 function randomAvatar() {
-    setInterval(() => {
-        const img = document.getElementById('random-avatar');
-        const preUrl = "https://robohash.org/";
-        const randomString = Math.random().toString(36).slice(2, 7);
-        const postUrl = "?set=set1";
-        img.src = preUrl + randomString + postUrl;
-    }, 30000);
+    getAvatar();
+    setInterval(getAvatar, 30000);
 }
 
+function getAvatar() {
+    const preUrl = "https://robohash.org/";
+    const randomString = Math.random().toString(36).slice(2, 7);
+    const postUrl = "?set=set1";
+
+    const holder = document.getElementById('avatar-holder');
+
+    let img = document.getElementById('random-avatar');
+
+    if (img === undefined) {
+        img = document.createElement('img');
+        img.src = preUrl + randomString + postUrl;
+        img.classList.add('mx-2');
+        img.width = "40";
+        img.height = "40";
+        img.style = "transform: scaleX(-1);";
+        img.id = "random-avatar";
+        holder.appendChild(img);
+    } else {
+        img.src = preUrl + randomString + postUrl;
+    }
+}
